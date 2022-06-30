d846911d8295d3969e08b399a667d1e02439e8f7
Phu <nguyenphu1147@gmail.com>
2022-06-30 09:26:26 +0700

    Admin Template

--- server/routes/web.php
+++ server/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AdminController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Login
+
+Route::get('login',[LoginController::class,'getlogin'])->name('getlogin');
+
+
+//Admin
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+    Route::get('index', [AdminController::class, 'index'])->name('index');
+    Route::get('basictable', [AdminController::class, 'basicTable'])->name('basicTable');
+    Route::get('flotchart', [AdminController::class, 'flotChart'])->name('flotChart');
+    Route::get('error', [AdminController::class, 'errorPage'])->name('errorPage');
+    Route::get('registration', [AdminController::class, 'registration'])->name('registration');
+});
+
